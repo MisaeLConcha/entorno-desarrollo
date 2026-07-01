746f93e20667f67a3c00942a83ee65966da7e527
Misael Concha <mis.concha@duocuc.cl>
2026-07-01 16:08:10 +0000

    Se añadieron correcciones al GlobalException para cubrir mas casos

--- codigo-fuente/pporden/src/main/java/com/duoc/pporden/exception/GlobalExceptionHandler.java
+++ codigo-fuente/pporden/src/main/java/com/duoc/pporden/exception/GlobalExceptionHandler.java
@@ -2,9 +2,13 @@ package com.duoc.pporden.exception;
 
 import com.duoc.pporden.dto.ErrorResponse;
 
+import feign.RetryableException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.time.LocalDateTime;
 
@@ -58,4 +62,57 @@ public class GlobalExceptionHandler {
             .status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(error);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> manejarValidacion(
+        MethodArgumentNotValidException ex) {
+
+    String mensaje = ex.getBindingResult()
+        .getFieldErrors()
+        .stream()
+        .map(error -> error.getDefaultMessage())
+        .findFirst()
+        .orElse("Error de validacion");
+
+    ErrorResponse error =
+        new ErrorResponse(
+            LocalDateTime.now(),
+            HttpStatus.BAD_REQUEST.value(),
+            mensaje);
+
+    return ResponseEntity
+        .badRequest()
+        .body(error);
+    }
+
+    @ExceptionHandler(RetryableException.class)
+    public ResponseEntity<ErrorResponse> manejarServicioNoDisponible(
+        RetryableException ex) {
+
+    ErrorResponse error =
+        new ErrorResponse(
+            LocalDateTime.now(),
+            HttpStatus.SERVICE_UNAVAILABLE.value(),
+            "El servicio externo no está disponible en este momento");
+
+    return ResponseEntity
+        .status(HttpStatus.SERVICE_UNAVAILABLE)
+        .body(error);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> manejarJsonIncorrecto(
+        HttpMessageNotReadableException ex){
+
+    ErrorResponse error =
+        new ErrorResponse(
+            LocalDateTime.now(),
+            HttpStatus.BAD_REQUEST.value(),
+            "El cuerpo de la solicitud contiene datos inválidos.");
+
+    return ResponseEntity
+        .badRequest()
+        .body(error);
+    }
+
 }
